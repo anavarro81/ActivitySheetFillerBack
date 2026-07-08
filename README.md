3d53063c6913948921f69e9bd42470d4eee6cc0d
Antonio Navarro <dojeDulDev@Proton.me>
2026-07-08 15:48:30 +0200

    feat: set up basic project configuration with express

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ActivitySheetFillerBack
-Backend en Node. Aplicación que permite rellenar la hoja de actividad semanal de las practicas con las tareas realizadas cada semana y generar un documento de Word descargable con las mismas. 
+Aplicación que permite rellenar la hoja de actividad semanal de las prácticas con las tareas realizadas cada semana y generar un documento de Word descargable con las mismas. 
+
+
---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+//import { connect } from "./src/bd.js";
+
+// import { userRoutes } from "./src/routes/user.routes.js";
+
+const app = express();
+
+// Conecta a la BBDD
+//connect();
+
+app.use(
+  cors({
+    origin: "*",
+    credential: true,
+  }),
+);
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
