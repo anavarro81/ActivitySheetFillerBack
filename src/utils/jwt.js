3c65df9c43e996471639f77fbe739e62d705cd3d
Antonio Navarro <dojeDulDev@Proton.me>
2026-07-21 15:11:20 +0200

    feat: retrieve DNI and ID from token

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -4,8 +4,8 @@ import jwt from "jsonwebtoken"
 // Genera un JSON Web Token (JWT)
 // Recibe como parametros el id del usuario y su email. 
 
-export const generateSign = (id, email) => {
-    return jwt.sign({id, email}, process.env.JWT_KEY, {expiresIn: "1h"})
+export const generateSign = (id, dni) => {
+    return jwt.sign({id, dni}, process.env.JWT_KEY, {expiresIn: "1h"})
 }
 
 // Devuelve true o false en funcion de si el token enviado en válido o no. 
