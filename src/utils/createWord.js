6365fc34077e1a2e585f9571fee2269254417d93
Antonio Navarro <dojeDulDev@Proton.me>
2026-09-10 17:13:12 +0200

    feat: upload Word template to Cloudinary for Vercel fetch integration

--- src/utils/createWord.js
+++ src/utils/createWord.js
@@ -1,17 +1,40 @@
 import { createReport } from "docx-templates";
 import fs from "fs";
+import path from 'path';
 
 export const createWordDocument = async (wordData) => {
   const { name, lastname, internship_period, daily_logs } = wordData;
 
   const templatePath = "wordTemplate.docx";
 
-  if (!fs.existsSync(templatePath)) {
-    console.error(`No se encontró la plantilla ${templatePath} `);
-    throw new Error("Template not found");
+  const isProd = process.env.NODE_ENV === "production";
+  const URL_TEMPLATE =
+    process.env.TEMPLATE_URL ||
+    "https://res.cloudinary.com/dxm9rdtus/raw/upload/v1789051512/wordTemplate_nksqcx.docx";
+  let template;
+
+  if (isProd) {
+    const response = await fetch(URL_TEMPLATE);
+
+    console.log('response ', response)
+
+    if (!response.ok) {
+      throw new Error(`No se pudo cargar la plantilla: ${response.status}`);
+    }
+
+    template = Buffer.from(await response.arrayBuffer());
+
+    console.log('template ', template)
+
+  } else {
+    const templatePath = path.resolve(process.cwd(), "wordTemplate.docx");
+    if (!fs.existsSync(templatePath)) {
+      throw new Error(`Template not found: ${templatePath}`);
+    }
+    template = fs.readFileSync(templatePath);
   }
 
-  const template = fs.readFileSync(templatePath);
+
 
   const templateData = {
     name,
@@ -19,9 +42,6 @@ export const createWordDocument = async (wordData) => {
     internship_period,
     daily_logs,
   };
-
-  
-
 
   const buffer = await createReport({
     template,
